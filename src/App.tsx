5565faa044d24c7bc5863f9fb52725cafb4f752a
Najim Rafiqi <najim.rafiqi@cdbdx.biz>
2020-05-07 12:23:15 +0200

    End of section 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: [],
+  };
+  componentDidMount() {
+    this.setState({
+      values: [
+        { id: 1, name: "Values 101" },
+        { id: 2, name: "Values 102" },
+        { id: 3, name: "Values 103" },
+      ],
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>
+                {value.id} - {value.name} {"OKOK"}
+              </li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
